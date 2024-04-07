cd4864a1b401a212da2027d5019c545c28b30e7c
Kutluhan Ünsal <kutluhan.unsal@hotmail.com>
2024-04-08 00:45:27 +0300

    Create alexa.py

---
+++ alexa.py
@@ -0,0 +1,82 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+
+# 
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'alexa' in command:
+                command = command.replace('alexa', '')
+                print(command)
+    except:
+        pass
+    return command
+
+
+def run_alexa():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing ' + song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        talk('Current time is ' + time)
+    elif 'who the heck is' in command:
+        person = command.replace('who the heck is', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    elif 'show me something interesting' in command:
+        talk("your mom")
+    elif 'funny' in command:
+        talk("your mom")
+    elif 'small' in command:
+        talk("that's what she said")
+
+    elif 'date' in command:
+        talk('sorry, I have a headache')
+    elif 'are you single' in command:
+        talk('I am in a relationship with wifi')
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
+    elif 'i feel so bad' in command:
+        talk("if you feel bad I am gonna fuck your ass then I am gonna bite your ass")
+    elif 'can i take a coffe' in command:
+         talk("your mom maybe can give one")
+    else:
+        talk('Please say the command again.')
+
+
+a = input("TO START (s)\nTO FINISH (f) :")
+while True:
+    if a == 's':
+        try:
+            run_alexa()
+        except UnboundLocalError:
+            print("No command detected! Alexa has stopped working ")
+            break
+    else:
+        break
+
+
+
